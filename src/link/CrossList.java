1da367605b4d2e010e3833af8d55156b4bfde118
fernfei <fernfei@china.com>
2022-07-07 23:41:25 +0800

    203. 移除链表元素

--- src/link/CrossList.java
+++ src/link/CrossList.java
@@ -2,53 +2,66 @@ package link;
 
 /**
  * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
- *
+ * <p>
  * 图示两个链表在节点 c1 开始相交：
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 题目数据 保证 整个链式结构中不存在环。
- *
+ * <p>
  * 注意，函数返回结果后，链表必须 保持其原始结构 。
- *
+ * <p>
  * 自定义评测：
- *
+ * <p>
  * 评测系统 的输入如下（你设计的程序 不适用 此输入）：
- *
+ * <p>
  * intersectVal - 相交的起始节点的值。如果不存在相交节点，这一值为 0
  * listA - 第一个链表
  * listB - 第二个链表
  * skipA - 在 listA 中（从头节点开始）跳到交叉节点的节点数
  * skipB - 在 listB 中（从头节点开始）跳到交叉节点的节点数
  * 评测系统将根据这些输入创建链式数据结构，并将两个头节点 headA 和 headB 传递给你的程序。如果程序能够正确返回相交节点，那么你的解决方案将被 视作正确答案 。
- *
+ * <p>
  *  
- *
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode.cn/problems/intersection-of-two-linked-lists
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * @author hufei
  */
 public class CrossList {
 
     public static void main(String[] args) {
-        link.ListNode nodeA1 = new link.ListNode(9);
-//        link.ListNode nodeA2 = new link.ListNode(2);
-//        link.ListNode nodeA3 = new link.ListNode(3);
-        link.ListNode nodeB1 = new link.ListNode(1);
-//        link.ListNode nodeB2 = new link.ListNode(2);
-        link.ListNode node8 = new link.ListNode(8);
-//        nodeA1.next = node8;
-//        nodeA2.next = nodeA3;
-//        nodeA3.next = node8;
-        nodeB1.next = nodeA1;
-        //nodeB2.next = node8;
-
-        ListNode intersectionNode = getIntersectionNode2(nodeA1, nodeB1);
-        System.out.printf("");
+//        link.ListNode nodeA1 = new link.ListNode(9);
+////        link.ListNode nodeA2 = new link.ListNode(2);
+////        link.ListNode nodeA3 = new link.ListNode(3);
+//        link.ListNode nodeB1 = new link.ListNode(1);
+////        link.ListNode nodeB2 = new link.ListNode(2);
+//        link.ListNode node8 = new link.ListNode(8);
+////        nodeA1.next = node8;
+////        nodeA2.next = nodeA3;
+////        nodeA3.next = node8;
+//        nodeB1.next = nodeA1;
+//        //nodeB2.next = node8;
+//
+//        ListNode intersectionNode = getIntersectionNode2(nodeA1, nodeB1);
+//        System.out.printf("");
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(6);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        ListNode listNode = removeElements(node1, 3);
+        System.out.println();
     }
+
     public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         // A B链表长度
         ListNode tempA = headA;
@@ -104,10 +117,22 @@ public class CrossList {
         // 1 2 3 8 1 3 8
         ListNode tempA = headA;
         ListNode tempB = headB;
-        while (tempA !=tempB) {
+        while (tempA != tempB) {
             tempA = tempA == null ? headB : tempA.next;
             tempB = tempB == null ? headA : tempB.next;
         }
         return tempA;
+    }
+
+    public static ListNode removeElements(ListNode head, int val) {
+        // 1 2 3 3 6
+        if (head == null) {
+            return null;
+        }
+        head.next = removeElements(head.next, val);
+        if (head.val == val) {
+            head = head.next;
+        }
+        return head;
     }
 }

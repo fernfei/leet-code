fe833f74e7a75e59c1c01f37a93b96e737dd9f14
fernfei <1639670695@qq.com>
2022-08-15 14:39:46 +0800

    判断字符是否是回文排列

--- src/bit/Solution.java
+++ src/bit/Solution.java
@@ -6,14 +6,20 @@ package bit;
  */
 public class Solution {
     public static void main(String[] args) {
-        int res = majorityElement(new int[]{2,1,3,2,3,2});
-        System.out.println(res);
+        // 多数元素测试
+        int res = majorityElement(new int[]{2, 1, 3, 2, 3, 2});
+        // 判断字符是否唯一测试
         boolean abcdefg = isUnique("abcdefg");
-        System.out.println(abcdefg);
+        // 判断字符串是否是排列回文
+        boolean code = canPermutePalindrome("9y");
+        // 反转二进制
+        int reverseBits = reverseBits(99999999);
+
     }
 
     /**
      * 找出多数元素
+     *
      * @param nums [3,1,3]
      * @return 3;
      */
@@ -31,11 +37,12 @@ public class Solution {
 
     /**
      * 判断字符串是否是唯一的
+     *
      * @param astr "abcdefg"
      * @return true
      */
     public static boolean isUnique(String astr) {
-        if(astr.length() > 26) {
+        if (astr.length() > 26) {
             return false;
         }
         boolean[] bit = new boolean[26];
@@ -52,6 +59,7 @@ public class Solution {
 
     /**
      * 判断字符串是否是唯一的
+     *
      * @param astr "abcdefg"
      * @return true
      */
@@ -61,6 +69,7 @@ public class Solution {
 
     /**
      * 判断字符串是否是唯一的
+     *
      * @param astr "abcdefg"
      * @return true
      */
@@ -74,5 +83,46 @@ public class Solution {
             }
         }
         return true;
+    }
+
+    /**
+     * 判断字符串是否是排列回文
+     *
+     * @param s 字符串
+     * @return true是 false否
+     */
+    public static boolean canPermutePalindrome(String s) {
+        long front = 0, end = 0;
+        for (char c : s.toCharArray()) {
+            if (c > 64) {
+                front ^= 1L << (c - 64);
+            } else {
+                end ^= 1L << c;
+            }
+        }
+        return Long.bitCount(front) + Long.bitCount(end) <= 1;
+    }
+
+    private static final int M1 = 0x55555555; // 01010101010101010101010101010101
+    private static final int M2 = 0x33333333; // 00110011001100110011001100110011
+    private static final int M4 = 0x0f0f0f0f; // 00001111000011110000111100001111
+    private static final int M8 = 0x00ff00ff; // 00000000111111110000000011111111
+
+    public static int reverseBits(int n) {
+        n = n >>> 1 & M1 | (n & M1) << 1;
+        n = n >>> 2 & M2 | (n & M2) << 2;
+        n = n >>> 4 & M4 | (n & M4) << 4;
+        n = n >>> 8 & M8 | (n & M8) << 8;
+        return n >>> 16 | n << 16;
+    }
+
+    public static int reverseBits2(int n) {
+        int rev = 0;
+        for (int i = 0; i < 32; i++) {
+            // 00 & 1
+            rev = rev << 1 & n | 0;
+
+        }
+        return 1;
     }
 }

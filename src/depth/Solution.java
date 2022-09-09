cfc1557985b79252c02f899f4c4f365f9fa992d7
fernfei <1639670695@qq.com>
2022-09-09 16:13:00 +0800

    恢复搜索二叉树

--- src/depth/Solution.java
+++ src/depth/Solution.java
@@ -20,7 +20,13 @@ public class Solution {
         // 判断两个二叉树是否相等
         boolean sameTree = isSameTree(new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(2, new TreeNode(4), new TreeNode(3)));
 
-        isBalanced(new TreeNode(1, new TreeNode(2,new TreeNode(3),null), new TreeNode(2)));
+        isBalanced(new TreeNode(1, new TreeNode(2, new TreeNode(3), null), new TreeNode(2)));
+
+        postorderTraversal3(new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null)));
+
+        isSymmetric2(new TreeNode(1, new TreeNode(2), new TreeNode(2)));
+
+        recoverTree(new TreeNode(1, new TreeNode(3, null, new TreeNode(2)), null));
     }
 
     /**
@@ -91,7 +97,39 @@ public class Solution {
     }
 
     /**
-     * 判断二叉树是否是轴对称
+     * 判断二叉树是否是轴对称 - 迭代
+     *
+     * @param root 二叉树
+     * @return true 轴对称
+     */
+    public static boolean isSymmetric2(TreeNode root) {
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+        while (!queue.isEmpty()) {
+            TreeNode left = queue.removeFirst();
+            TreeNode right = queue.removeFirst();
+            if (left == null && right == null) {
+                continue;
+            }
+            if (left == null || right == null) {
+                return false;
+            }
+            if (left.val != right.val) {
+                return false;
+            }
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+        return true;
+    }
+
+
+    /**
+     * 判断二叉树是否是轴对称 - 递归
+     *
      * @param root 二叉树
      * @return true 轴对称
      */
@@ -99,9 +137,11 @@ public class Solution {
         return isSymmetric(root.left, root.right);
     }
 
+
     /**
      * 判断二叉树是否是轴对称
-     * @param left 左二叉树
+     *
+     * @param left  左二叉树
      * @param right 右二叉树
      * @return true 轴对称
      */
@@ -139,7 +179,8 @@ public class Solution {
 
     /**
      * 路径总和-根节点到叶子节点总和==目标值
-     * @param root 二叉树
+     *
+     * @param root      二叉树
      * @param targetSum 目标值
      * @return 是否存在结果
      */
@@ -173,6 +214,7 @@ public class Solution {
 
     /**
      * 后序遍历
+     *
      * @param root 二叉树
      * @return 结果
      */
@@ -182,23 +224,70 @@ public class Solution {
         return res;
     }
 
+
+    public static List<Integer> postorderTraversal3(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        TreeNode prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.right == null || root.right == prev) {
+                res.add(root.val);
+                prev = root;
+                root = null;
+            } else {
+                stack.push(root);
+                root = root.right;
+            }
+        }
+        return res;
+    }
+
     /**
      * 后序遍历-迭代
+     *
      * @param root 二叉树
      * @return res 后序遍历结果
      */
     public List<Integer> postorderTraversal2(TreeNode root) {
+        //  1
+        //      2
+        //3
+        List<Integer> res = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
+        TreeNode preNode = null;
         while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.right == null || root.right == preNode) {
+                preNode = root;
+                res.add(preNode.val);
+                root = null;
+            } else {
+                root = root.right;
+                stack.push(root);
+            }
 
         }
-        return null;
+        return res;
     }
 
     /**
      * 后序遍历-递归
+     *
      * @param root 二叉树
-     * @param res 后序遍历结果
+     * @param res  后序遍历结果
      */
     public void postorderTraversal(TreeNode root, List<Integer> res) {
         if (root == null) {
@@ -208,6 +297,76 @@ public class Solution {
         postorderTraversal(root.right, res);
         res.add(root.val);
     }
+
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        long preVal = Long.MIN_VALUE;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            TreeNode pop = stack.pop();
+            if (pop.val <= preVal) {
+                return false;
+            }
+            root = pop.right;
+            preVal = pop.val;
+        }
+        return true;
+    }
+
+    public boolean isValidBST2(TreeNode root) {
+        return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+
+    public boolean isValidBST(TreeNode root, int lower, int upper) {
+        if (root == null) {
+            return true;
+        }
+        // 3 > +inf || 3 < 2
+        if (root.val >= upper || root.val <= lower) {
+            return false;
+        }
+        // 2 1 3
+        return isValidBST(root.left, lower, root.val) && isValidBST(root.right, root.val, upper);
+    }
+
+    /**
+     * 恢复搜索二叉树
+     * @param root 二叉树
+     */
+    public static void recoverTree(TreeNode root) {
+        // 中序遍历 4 2 3 1     1 4 2
+        // 中序遍历 1 2 3 4     1 2 4
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode f = null, s = null, pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            // 1 2 3 4 9 7 8 6 10
+            TreeNode pop = stack.pop();
+            if (pre != null && pre.val > pop.val) {
+                if (f == null) {
+                    f = pre;
+                }
+                s = pop;
+            }
+            pre = pop;
+            root = pop.right;
+        }
+        if (f != null && s != null) {
+            // f = 1 s = 2
+            f.val ^= s.val;
+            s.val ^= f.val;
+            f.val ^= s.val;
+
+        }
+    }
+
 }
 
 
